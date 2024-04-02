dd8016b6b9700388267e0a8579d1e9839ae12a87
Trainee <trainee@finehomeint.com>
2024-04-02 13:24:14 +0400

    build

--- main.go
+++ main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"database/sql"
+	"log"
+	"reflect"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"tutorial.sqlc.dev/app/tutorial"
+)
+
+func run() error {
+	ctx := context.Background()
+
+	db, err := sql.Open("mysql", "root@/mysql-golang?parseTime=true")
+	if err != nil {
+		return err
+	}
+
+	queries := tutorial.New(db)
+
+	// list all authors
+	authors, err := queries.ListAuthors(ctx)
+	if err != nil {
+		return err
+	}
+	log.Println(authors)
+
+	// create an author
+	result, err := queries.CreateAuthor(ctx, tutorial.CreateAuthorParams{
+		Name: "Brian Kernighan",
+		Bio:  sql.NullString{String: "Co-author of The C Programming Language and The Go Programming Language", Valid: true},
+	})
+	if err != nil {
+		return err
+	}
+
+	insertedAuthorID, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+	log.Println(insertedAuthorID)
+
+	// get the author we just inserted
+	fetchedAuthor, err := queries.GetAuthor(ctx, insertedAuthorID)
+	if err != nil {
+		return err
+	}
+
+	// prints true
+	log.Println(reflect.DeepEqual(insertedAuthorID, fetchedAuthor.ID))
+	return nil
+}
 
 func main() {
-	fmt.Printf("%d\n", 3)
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
 }
